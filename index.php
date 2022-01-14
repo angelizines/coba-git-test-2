b8c22ef56be8e7ee99ddcf4e1e08347aee412f3f
Angelizines <angelita_j20@mhs.istts.ac.id>
2022-01-14 12:02:58 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque hic laudantium alias sunt porro fugit veritatis
         iste officiis numquam sint.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
